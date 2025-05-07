abd8fc3454e7f90a1834608b0eb144cf733497f2
khuship1409 <khushipatel140904@gmail.com>
2025-05-07 22:26:28 +0530

    Daily work-7 may

--- Array_20_Questions/Question3.java
+++ Array_20_Questions/Question3.java
@@ -1,32 +1,46 @@
 /*Find the kth max and min element of an array */
-
-import java.util.Scanner;
-
-class Question3 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int[] x={56,8,12,4,77,32,69,58,15,26,78};
-        int k=sc.nextInt();
-        int max=x[0];
-        int max1=x[0];
-
-        for(int i=0;i<x.length;i++){
-            if (max<x[i]) {
-                for(int j=max;j<k;j++){
-                    if(max1<x[j]){
-                        max1=x[j];
-                    }
-
+public class Question3 {
+   void bubbleSort(int arr[]){
+        int n=arr.length;
+        for(int i = 0 ; i < n - 1 ; i++ ){
+            for(int j = 0 ; j < n - i - 1 ; j++){
+            if ( arr[ j ] > arr[ j + 1 ]) {
+                 int temp=arr[j];
+                 arr[j]=arr[j+1];
+                 arr[j+1]=temp;
                 }
-
-                
             }
-
         }
-
-        System.out.println(max1);
-
-        
     }
-    
+    void showArray(int arr[]){
+        for(int value : arr ){
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+    void kthmaxmin(int arr[],int k){
+        int n = arr.length;
+        
+        if ( k>0 && k<n) {
+        int kthmax=arr[n-k];
+        System.out.println("the "+k+"th maximum element is- "+ kthmax);
+
+        int kthmin=arr[k-1];
+        System.out.println("the "+k+"th minimum element is- "+ kthmin);
+        }
+        else{
+            System.out.println("invalid k");
+        }
+    }
+    public static void main(String[] args) {
+        Question3 y=new Question3();
+       int[] x={56, 8, 12, 4, 77, 32, 69, 58, 15, 26, 78};
+       System.out.println("the given array is - ");
+       y.showArray(x);
+       y.bubbleSort(x);
+       System.out.println("the sorted array is - ");
+       y.showArray(x);
+       y.kthmaxmin(x,3);
+    }   
 }
+
